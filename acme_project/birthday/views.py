2ca618f163d9ac1c46e9617d0bec3fe03cb8a2c1
danilafinkt <d.finkt@yandex.com>
2025-01-28 17:45:32 +0300

    3

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 from .forms import BirthdayForm
 
+from .utils import calculate_birthday_countdown
+
 
 def birthday(request):
-    form = BirthdayForm()
+    form = BirthdayForm(request.GET or None)
     context = {'form': form}
-    return render(request, 'birthday/birthday.html', context=context)
+    if form.is_valid():
+        birthday_countdown = calculate_birthday_countdown(
+            # ...и передаём в неё дату из словаря cleaned_data.
+            form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)

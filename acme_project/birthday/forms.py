ecea55d7e841d91d07ed60a1d6fe4e2b37d0792f
danilafinkt <d.finkt@yandex.com>
2025-01-31 14:04:10 +0300

    8

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -14,7 +14,11 @@ class BirthdayForm(forms.ModelForm):
     class Meta:
         model = Birthday
         fields = '__all__'
-        widgets = {'birthday': forms.DateInput(attrs={'type': 'date'})}
+        widgets = {
+            'birthday': forms.DateInput(
+                attrs={'type': 'date'}, format='%Y-%m-%d'
+            )
+        }
 
     def clean_first_name(self):
         first_name = self.cleaned_data['first_name']

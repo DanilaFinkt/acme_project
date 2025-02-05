02a0b76f6e1b7d69153f302a8a9cf044e3960e2e
Danila Finkt <d.finkt@yandex.com>
2025-02-05 20:48:50 +0300

    15

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -24,3 +24,7 @@ urlpatterns = [
 ]
 handler404 = 'core.views.page_not_found'
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    import debug_toolbar
+    # Добавить к списку urlpatterns список адресов из приложения debug_toolbar:
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
